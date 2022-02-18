826c60e653e81b42fe8824bab5562dca09b0076d
Artyom Sazonov <sazonau.artiom@yandex.by>
2022-02-18 12:32:08 +0300

    my store

---
+++ src/app/shipping/shipping.component.ts
@@ -0,0 +1,18 @@
+import { CartService } from './../cart.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.css']
+})
+export class ShippingComponent implements OnInit {
+  shippingCosts = this.cartService.getShippingPrice()
+  constructor(
+    private cartService: CartService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
